1d2f9b6ec97466e7cea354ec2a538d0318faf0f7
vigo walker <vigopaul05@gmail.com>
2026-04-26 14:28:36 +0200

    Clear SSID configuration for security reasons

--- AthenaV2/main.py
+++ AthenaV2/main.py
@@ -10,7 +10,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 def main():
     # ── CONFIGURATION ──
     config = BotConfig(
-        ssid=r'42["auth",{"session":"vtftn12e6f5f5008moitsd6skl","isDemo":1,"uid":27658142,"platform":2,"isFastHistory":true,"isOptimized":true}]',  # <--- PUT YOUR SSID HERE
+        ssid=r'',  # <--- PUT YOUR SSID HERE
         asset="AUDNZD_otc",
         timeframe=60,            # 1 minute candles
         default_expiry=55,      # will be overridden by AI selector
